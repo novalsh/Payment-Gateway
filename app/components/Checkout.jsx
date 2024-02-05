73d2d6d2273ab5d6d16386e1327f729ce5c8a7fe
novalsh <muhammad.harjana@binus.ac.id>
2024-02-06 05:14:37 +0700

    first commit

---
+++ app/components/Checkout.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { product } from "../libs/product";
+
+const Checkout = () => {
+  const [quantity, setQuantity] = useState(1);
+
+  const decreaseQuantity = () => {
+    setQuantity((prevState) => (quantity > 1 ? prevState - 1 : null));
+  };
+
+  const increaseQuantity = () => {
+    setQuantity((prevState) => prevState + 1);
+  };
+
+  const checkout = async () => {
+    const data = {
+      id: product.id,
+      productName: product.name,
+      price: product.price,
+      quantity: quantity,
+    };
+
+    const response = await fetch("/api/tokenizer", {
+      method: "POST",
+      body: JSON.stringify(data),
+    });
+
+    const requestData = await response.json();
+    console.log({ requestData });
+  };
+
+  const generatePaymentLink = async () => {
+    alert("Checkout Payment Link! 🔥");
+  };
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <div className="flex sm:gap-4">
+          <button
+            className="transition-all hover:opacity-75"
+            onClick={decreaseQuantity}
+          >
+            ➖
+          </button>
+
+          <input
+            type="number"
+            id="quantity"
+            value={quantity}
+            className="h-10 w-16 text-black border-transparent text-center"
+            onChange={(e) => setQuantity(e.target.value)}
+          />
+
+          <button
+            className="transition-all hover:opacity-75"
+            onClick={increaseQuantity}
+          >
+            ➕
+          </button>
+        </div>
+        <button
+          className="rounded bg-indigo-500 p-4 text-sm font-medium transition hover:scale-105"
+          onClick={checkout}
+        >
+          Checkout
+        </button>
+      </div>
+      <button
+        className="text-indigo-500 py-4 text-sm font-medium transition hover:scale-105"
+        onClick={generatePaymentLink}
+      >
+        Create Payment Link
+      </button>
+    </>
+  );
+};
+
+export default Checkout;
